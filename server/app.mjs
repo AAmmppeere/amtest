7521c17535a8e706084d0610166c2fadef0d181a
AmtestRepo <toyota.supa1991.com>
2024-08-03 19:49:58 +0700

    fix:deployment

--- server/app.mjs
+++ server/app.mjs
@@ -28,7 +28,7 @@ const port = process.env.PORT || 4000;
 
 const allowedOrigins = [
   process.env.VITE_BACKEND_URL,
-  `${import.meta.env.VITE_HOME_SERVICES_URL}`
+  `amtest-tau.vercel.app`
 ];
 
 const corsOptions = {
