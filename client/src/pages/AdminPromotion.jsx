cc2c5cc54ee5e0a05407d1a2b52373fd1723c8ca
AmtestRepo <toyota.supa1991.com>
2024-08-03 12:51:10 +0700

    remove link localhost

--- client/src/pages/AdminPromotion.jsx
+++ client/src/pages/AdminPromotion.jsx
@@ -67,7 +67,7 @@ function AdminPromotion() {
   const handleDeleteConfirm = async () => {
     try {
       await axios.delete(
-        `http://localhost:4000/promotion/${itemToDelete.promo_id}`
+        `${import.meta.env.VITE_API_BASE_URL}/promotion/${itemToDelete.promo_id}`
       );
       setOriginalPromotionCode((prevItems) =>
         prevItems.filter(
@@ -90,7 +90,7 @@ function AdminPromotion() {
 
   const getPromotionCode = async () => {
     try {
-      const result = await axios.get(`http://localhost:4000/promotion`);
+      const result = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/promotion`);
       console.log("Fetched promotion codes:", result.data.data);
       setOriginalPromotionCode(result.data.data);
     } catch (error) {
@@ -464,7 +464,7 @@ export default AdminPromotion;
 //   const handleDeleteConfirm = async () => {
 //     try {
 //       await axios.delete(
-//         `http://localhost:4000/promotion/${itemToDelete.promo_id}`
+//         `${import.meta.env.VITE_API_BASE_URL}/promotion/${itemToDelete.promo_id}`
 //       );
 //       setOriginalPromotionCode((prevItems) =>
 //         prevItems.filter(
@@ -487,7 +487,7 @@ export default AdminPromotion;
 
 //   const getPromotionCode = async () => {
 //     try {
-//       const result = await axios.get(`http://localhost:4000/promotion`);
+//       const result = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/promotion`);
 //       console.log("Fetched promotion codes:", result.data.data);
 //       setOriginalPromotionCode(result.data.data);
 //     } catch (error) {
